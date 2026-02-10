741025f179016af5966365224dbddfc517c8ef54
totto2727 <kaihatu.totto2727@gmail.com>
2026-02-11 01:14:54 +0900

    style(nvim): fix indentation in lazy.nvim bootstrap

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,14 +1,14 @@
 -- https://github.com/AstroNvim/AstroNvim?tab=readme-ov-file#minimal-confignviminitlua
 local lazy_path = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazy_path) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazy_path
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazy_path
+    })
 end
 vim.opt.rtp:prepend(lazy_path)
 
